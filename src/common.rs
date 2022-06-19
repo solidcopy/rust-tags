f8468724abe89df255c7704248555d9cb25a7a41
yuu <dev@solidcopy.net>
2022-06-20 02:01:44 +0900

    Macだとtagsファイルのパスが絶対パスでないとフォルダが参照できなかった。

--- src/common.rs
+++ src/common.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use once_cell::sync::Lazy;
 
 /// 共通の戻り値型
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-/// tagsファイルのパス
-pub static TAGS_FILEPATH: Lazy<&Path> = Lazy::new(|| Path::new("tags"));
+/// tagsファイル名
+pub static TAGS_FILEPATH: Lazy<PathBuf> = Lazy::new(|| Path::new("tags").canonicalize().unwrap());
 
 /// 対象フォルダパス
 pub static TARGET_FOLDER: Lazy<&Path> = Lazy::new(|| Path::new("."));
